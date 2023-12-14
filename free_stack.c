48b5b43ac0bcd8f95f6f92647d8e4b051ed0ce96
Sundus Mahmoud <132394446+Sundushigazey@users.noreply.github.com>
2023-12-14 21:51:18 +0200

    Create free_stack.c

---
+++ free_stack.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+* _emptys - empty the d linked list
+* @top: the head of the list
+*/
+void _emptys(stack_t *top)
+{
+	stack_t *t;
+
+	t = top;
+	while (top)
+	{
+		t = top->next;
+		free(top);
+		top = t;
+	}
+}
