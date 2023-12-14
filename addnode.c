d69cb86a4c7b2dcd59f51ecaf587f653f792b6af
Sundus Mahmoud <132394446+Sundushigazey@users.noreply.github.com>
2023-12-14 21:50:50 +0200

    Create addnode.c

---
+++ addnode.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * add_head - adds node
+ * @h: head of the stack
+ * @n: the added new val
+ * Return: 0
+*/
+void add_head(stack_t **h, int l)
+{
+
+	stack_t *add_new, *temp;
+
+	temp = *h;
+	add_new = malloc(sizeof(stack_t));
+	if (add_new == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (temp)
+		temp->prev = add_new;
+	add_new->l = l;
+	add_new->next = *h;
+	add_new->prev = NULL;
+	*h = add_new;
+}
