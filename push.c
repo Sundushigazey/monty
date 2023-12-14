a9b6ec3aa0831a5a378a1c695aabceb732ff65ee
Sundus Mahmoud <132394446+Sundushigazey@users.noreply.github.com>
2023-12-14 21:55:27 +0200

    Create push.c

---
+++ push.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * _push - add new node at top of stk
+ * @top: head of th list
+ * @count_line: to count num line
+*/
+void _push(stack_t **top, unsigned int count_line)
+{
+    if (!bus.arg)
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", count_line);
+        fclose(bus.file);
+        free(bus.content);
+        _emptys(*top);
+        exit(EXIT_FAILURE);
+    }
+
+    int l = atoi(bus.arg);
+    add_head(top, l);
+}
