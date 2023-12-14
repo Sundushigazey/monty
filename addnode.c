b8eadf41cbe73d5cd47670c629ba772a936e619b
Sundus Mahmoud <132394446+Sundushigazey@users.noreply.github.com>
2023-12-14 22:23:55 +0200

    Update addnode.c

--- addnode.c
+++ addnode.c
@@ -2,7 +2,7 @@
 /**
  * add_head - adds node
  * @h: head of the stack
- * @l: the added new val
+ * @n: the added new val
  * Return: 0
 */
 void add_head(stack_t **h, int l)
@@ -13,11 +13,11 @@ add_new = malloc(sizeof(stack_t));
 if (add_new == NULL)
 {
 printf("Error\n");
-exit(0);
+exit(EXIT_FAILURE);
 }
 if (temp)
 temp->prev = add_new;
-add_new->l = l;
+add_new->n = l;
 add_new->next = *h;
 add_new->prev = NULL;
 *h = add_new;
